f7501e66659c5889c86762e8d9a78e9815b42478
Mohamed Wael <hfhybujj@gmail.com>
2025-01-28 23:16:39 +0200

    commit

--- helpers/db.js
+++ helpers/db.js
@@ -1,19 +1,26 @@
 const mongoose = require("mongoose");
 
+let isConnected;
+const connectDB = async () => {
+  if (isConnected) {
+    console.log("Using existing MongoDB connection");
+    return;
+  }
 
-const uri = process.env.MONGO_URI;
-console.log(uri);
-async function connectDB() {
   try {
-    await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("Connected to MongoDB Atlas");
+    const uri = process.env.MONGO_URI;
+    if (!uri) {
+      throw new Error("MONGO_URI is not defined in environment variables.");
+    }
+
+    const db = await mongoose.connect(uri);
+
+    isConnected = db.connections[0].readyState;
+    console.log("MongoDB connected successfully!");
   } catch (error) {
-    console.error("Error connecting to MongoDB Atlas:", error.message);
+    console.error(`Error connecting to MongoDB Atlas: ${error.message}`);
     process.exit(1);
   }
-}
+};
 
 module.exports = connectDB;
