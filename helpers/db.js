6976fb19527f2585240abf0294e8df106e0336ec
Mohamed Wael <hfhybujj@gmail.com>
2025-01-28 22:51:00 +0200

    Integrate MongoDB for user session management: add mongoose dependency, create database connection, and refactor user session handling to use MongoDB

---
+++ helpers/db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+
+const uri = process.env.MONGO_URI;
+async function connectDB() {
+  try {
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB Atlas");
+  } catch (error) {
+    console.error("Error connecting to MongoDB Atlas:", error.message);
+    process.exit(1);
+  }
+}
+
+module.exports = connectDB;
